1be8899caed05b8c76cdb7a0e4bea0e513d74622
Yu Taniguchi <kiji46nyan@gmail.com>
2026-07-06 12:53:32 +0900

    mc-tools/fix: change project name

--- cpp/mc-tools/run.sh
+++ cpp/mc-tools/run.sh
@@ -3,4 +3,4 @@
 mkdir -p build/clang
 cmake -S . -B build/clang -G Ninja -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
 cmake --build build/clang
-./bin/mcje_status_ping
+./bin/mctools_status_ping
